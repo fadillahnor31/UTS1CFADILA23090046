81271fa9d797b283c4a71033e212e7d5cb293d72
fadillah <fdillah155@gmail.com>
2023-11-10 14:11:52 +0700

    commit massage

---
+++ src/jawabanno3.java
@@ -0,0 +1,12 @@
+public class jawabanno3 {
+
+    private String fadlh;
+    public jawabanno3(){
+        // misalnya saya bikin coding buat jawaban spt ini
+        // km tinggal ke menu GIT
+    }
+}// km liat itu ada yang warna ijo ya, berati ada file baru yang belum
+// ada di github km, jadi kalo mau upload
+// ke github harus commit dulu
+// abis itu tekan commit and push
+// kalo muncul warning pilih commit and push anyway
