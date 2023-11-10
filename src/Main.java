e392a6036a433010ca349667064e93894ffd4c68
fadillah <fdillah155@gmail.com>
2023-11-10 14:04:58 +0700

    message commit okelah

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+
+        }
+    }
+
+}
+//intellij km saya disconnect dulu ya dari github, nti km coba konekin ulang
+// saya ajari dari layar
+ // silakan ke menu GIT, kalo g ada menu git silakan ke menu VCS
+// kita create dulu kalo project ini menggunakan GIT
+// pilih VCS - create GIT repository
+// nah kalo begini berarti km belum pernahinstall GIT
+// nti setelah ini kita setting dulu username dan password di git menggunakan terminal
+//pak ini saya ada apk git tapi beda mungkin ya pak? git pake yang downloadn, APK ga jalan di windows
+// APK jalan cuman di android ok
+// skr buka CMD di windows kita mau setting username dan email untuk git
+// cmd yang ini bukan pak
+// betul. trus ketik git config
+// tekan enter o
+// sudah pak. ketik lagi git config--global user.email "
+// sudah pak. Salah. km kurang teliti harusnya ada spasi di depan --global
+// sudah paktekan enter
